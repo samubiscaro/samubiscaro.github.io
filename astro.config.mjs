e940500c6ce6938a76d7e0bfd15525389ecfb9b9
Samuele Biscaro <samabarama@gmail.com>
2025-07-24 11:01:51 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samubiscaro.github.io',
-  base: 'my-repo',
+  //base: 'my-repo',
   integrations: [mdx(), sitemap(), tailwind()]
 })
 
