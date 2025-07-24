53a00c249309d30b3beb1c8351db52ad177a220f
Samuele Biscaro <samabarama@gmail.com>
2025-07-24 11:51:15 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
+//import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samubiscaro.github.io',
   //base: 'my-repo',
-  integrations: [mdx(), sitemap(), tailwind(), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }), ],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
