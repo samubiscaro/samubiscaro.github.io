11590359571dcab75c873f61ab6b5881424bfa2f
Samuele Biscaro <samabarama@gmail.com>
2025-07-24 11:19:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
